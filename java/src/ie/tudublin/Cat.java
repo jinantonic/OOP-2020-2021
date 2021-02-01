988d65c4d8cc5817d0d5c5ab45b064651b2f5a89
Jina Park <jinapark119@gmail.com>
2021-02-01 21:49:20 +0000

    killing the cat

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -2,8 +2,32 @@ package ie.tudublin;
 
 public class Cat extends Animal
 {
+    private int numLives;
+
     public Cat(String name)
     {
         super(name);
+        this.numLives = 9;
     }
+
+    // Need a getter method since the value is private
+    public int getNumLives()
+    {
+        return numLives;
+    }
+
+    public void kill()
+    {
+        if(numLives > 0)
+        {
+            numLives = numLives - 1;
+            System.out.println("Ouch!");
+        }
+
+        if(numLives == 0)
+        {
+            System.out.println("Dead");
+        }
+    }
+    
 }
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -9,6 +9,8 @@ public class Main
         Animal misty = new Dog("Misty");
 
         Animal topCat = new Cat("TopCat");
+        
+        Cat ginger = new Cat("Ginger");
 
         System.out.println(misty);
         System.out.println(topCat);
@@ -20,11 +22,9 @@ public class Main
         System.out.println(misty);
         System.out.println(topCat);
 
-        // What will get printed out??
-
-        // 1. topcat, Garfield
-        // 2. garfield, Garfield 
-
-
+        while(ginger.getNumLives()!= 0)
+        {
+            ginger.kill();
+        }
     }
-}
+} 
