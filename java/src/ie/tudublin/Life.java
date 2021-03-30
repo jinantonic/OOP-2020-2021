5acf9a9c4d0f573180e799969703ca2aa4b66ff7
Jina Park <jinapark119@gmail.com>
2021-03-30 17:24:25 +0100

    edit

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -13,14 +13,18 @@ public class Life extends PApplet
     // When you are specifying the elements of 2D array, it's apposite to scrren cordinates
     // For screen cordinates -> x is how far over you go, y is how far down you go screen
     // For 2D arrays -> row goes down to the screen, col goes across a screen
+    boolean[][] next = new boolean[size][size];
 
+
+    
+    
     public int countCellsAround(int row, int col)
     {
         int count = 0;
 
         // Use getCell here 
         // Check the cells around the current cell
-        if(getCell(board, row-1, col-1)) // top left
+        /*if(getCell(board, row-1, col-1)) // top left
         {
             count++;
         }
@@ -51,13 +55,13 @@ public class Life extends PApplet
         if(getCell(board, row+1, col+1)) // bottom right
         {
             count++;
-        }
+        }*/
 
-        /*for(int r = row - 1; r <= row + 1; r++)
+        for(int r = row - 1; r <= row + 1; r++)
         {
             for(int c = col - 1; c <= col + 1; c++)
             {
-                if(r != row && c != col) // Not counting ourselves
+                if(! (r == row && c == col)) // Not counting ourselves
                 {
                     if(getCell(board, r, c))
                     {
@@ -65,11 +69,9 @@ public class Life extends PApplet
                     }
                 }
             }
-        }*/
-        
+        }
         return count;
     }
-
 
 
     // Accesser methods -> allow us to query the boolean array and say whether the element is true or false
@@ -79,7 +81,7 @@ public class Life extends PApplet
         // Set the cell at position row and col to the value of b 
         // Bounce checking -> makes sure those 2 parameters don't go outside the range 
         // Making sure we can't write outside of the 2d array
-        if(row >= 0 && row < size - 1 && col >= 0 && col < size - 1)
+        if(row >= 0 && row < size && col >= 0 && col < size)
         {
             board[row][col] = b; // Assign the boolean value into the 2d array
         }
@@ -88,12 +90,15 @@ public class Life extends PApplet
     // Getter
     public boolean getCell(boolean[][] board, int row, int col) // It will returen boolean
     {   
-        if(row >= 0 && row < size - 1 && col >= 0 && col < size - 1) // Bounce checking
+        if(row >= 0 && row < size && col >= 0 && col < size ) // Bounce checking
         {
             // Return the cell at position row and col 
             return board[row][col];
         }
-        return false;
+        else
+        {
+            return false;
+        }
     }
 
     public void drawBoard(boolean[][] board)
@@ -114,14 +119,12 @@ public class Life extends PApplet
                 if(board[row][col]) // It's already a boolean expression so i don't need to say board == true
                 {
                     rect(x, y, cellSize, cellSize);
-                    fill(255);
+                    //fill(255);
                 }
-                
             }
         }
-
     }
-
+    
     
     private void printBoard(boolean[][] board)
     {
@@ -162,21 +165,56 @@ public class Life extends PApplet
         size(500, 500);
     }
 
+    public void clear() // Set all the cells as false
+    {
+        for(int row = 0; row < size; row++)
+        {
+            for(int col = 0; col < size; col++)
+            {
+                setCell(board, row, col, false);
+            }
+        }
+    }
+
+
     int mode = 0;
+    boolean paused = false;
     public void keyPressed() 
     {
-        // The value of mode will be the number of the numbe key pressed
-        if (keyCode >= '0' && keyCode <= '9')
-            mode = keyCode - '0';
+        if(keyCode == ' ')
+        {
+            paused = ! paused; // If paused is false, it will end up true
+        }
+        if(keyCode == '1')
+        {
+            randomize();
+        }
+        if(keyCode == '2')
+        {
+            clear();
+        }
+        if(keyCode == '3')
+        {
+            drawCross();
+        }
+    }
+
+    public void drawCross()
+    {
+        for(int i = 0; i < size; i++)
+        {
+            setCell(board, size / 2, i, true); // size/2 -> halfway upward -> set row halfway up the columns
+            setCell(board, i, size / 2, true); // size/2 -> halfway upward -> set col halfway up the rows
+        }
     }
 
     public void setup()
     {
         colorMode(RGB);
-        //randomize();
-        board[0][1] = true;
+        randomize();
+        /*board[0][1] = true;
         board[1][2] = true;
-        board[3][2] = true;
+        board[3][2] = true;*/
 
         println(countCellsAround(0, 2));
         
@@ -184,12 +222,72 @@ public class Life extends PApplet
 
         // Variable width does not get assigned until after we call size() so we put the cellSize here
         cellSize = width / (size);
+
+        frameRate(20); // 20 frames per second
     }
+
+    private void updateBoard()
+    {
+        for(int row = 0; row < size; row++)
+        {
+            for(int col = 0; col < size; col++)
+            {
+                int count = countCellsAround(row, col);
+                if(getCell(board, row, col)) // Alive cell
+                {
+                    if(count == 2 || count == 3)
+                    {
+                        setCell(next, row, col, true);
+                        //next[row][col] == true;
+                    }
+                    else
+                    {
+                        setCell(next, row, col, false);
+                        //next[row][col] == false;
+                    }
+                }
+                else // Dead cell
+                {
+                    if(count == 3)
+                    {
+                        setCell(next, row, col, true);
+                        //next[row][col] == true;
+                    }
+                    else
+                    {
+                        setCell(next, row, col, false);
+                        //next[row][col] == false;
+                    }
+                }
+            }
+        }
+
+        // Swap board and next
+        boolean[][] temp = board;
+        board = next;
+        next = temp;
+    }
+
+    // This method gets called automatically when the mouse is dragged across the screen
+    public void mouseDragged()
+    {
+        int row = (int) map(mouseY, 0, height, 0, size); // Function returns a float so i have to cast to an int 
+        // When the mouseY is at the top of the screen, I'm going to get row 0
+        // When the mouseY is all the way down bottom of the screen, I'm going to get the no. of rows
+        int col = (int) map(mouseX, 0, width, 0, size);
+        setCell(board, row, col, true); // Set the cells to be true and start the simulation 
+    }
+
 
     public void draw()
     {
         background(0);
         drawBoard(board); // Gets drawn every frame
+        if(! paused)
+        {
+            updateBoard();
+        }
+        
 
     }
 }
