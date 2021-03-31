5ab8d78fa88c5ee5ddace7e0c13a96608aad5805
Jina Park <jinapark119@gmail.com>
2021-03-31 16:20:29 +0100

    edit

--- java/src/ie/tudublin/Star.java
+++ java/src/ie/tudublin/Star.java
@@ -14,6 +14,36 @@ public class Star
     private float distance;
     private float xG, yG, zG;
     private float absMag;
+
+
+
+
+    // Constructor -> The method that gets called when the object gets created
+    // They don't have a return type 
+    public Star() // Defalut constructor -> constructor that takes no parameters
+    {
+
+    }
+
+    public Star(boolean hab, String displayName, float distance, float xG, float yG, float zG, float absMag)// Parameterised constructor which will take the default values for all of those fileds
+    {
+        // Now take all those values above and assign them to the fileds in the class
+        // We can disambiguate between the filed hab and the parameter hab by using the keyword, this
+        this.hab = hab;
+        this.displayName = displayName;
+        this.distance = distance;
+        this.xG = xG;
+        this.yG = yG;
+        this.zG = zG; 
+        this.absMag = absMag;// Now all of the fields are assigned 
+    }
+
+    
+
+
+
+
+
     public boolean isHab()
     {
         return hab;
@@ -70,5 +100,21 @@ public class Star
     public void setAbsMag(float absMag)
     {
         this.absMag = absMag;
+    }
+
+   
+
+   
+
+    // toString method is the method that gets called automatically whenever you pass this object to something that expects a string
+    // Source action -> generate toString()
+    /*public String toString()
+    {
+        return hab + "\t" + displayName + "\t" + distance + "\t" + xG + "\t" + yG + "\t" + zG + "\t" + absMag;
+    }*/
+    @Override
+    public String toString() {
+        return "Star [absMag=" + absMag + ", displayName=" + displayName + ", distance=" + distance + ", hab=" + hab
+                + ", xG=" + xG + ", yG=" + yG + ", zG=" + zG + "]";
     }
 }
--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -1,9 +1,28 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.data.Table;
+
 
 public class StarMap extends PApplet
 {
+    // Load an array list of start object
+    // Declare at the array list 
+    // crtl + . is automatic code fixing in VSC
+    // This is an empty arraylist that currently has no elements in it
+    ArrayList<Star> starts = new ArrayList<Star>(); // Array list holding the star object (generic -> passing a type to a parameter)
+
+    // Load the csv file into this arraylist 
+    void loadStars()
+    {
+        // Create a table object, table is one of the processing objects 
+        Table table = loadTable("HabHYG15ly.csv", "header"); // 1st -> name of the csv file, 2nd -> tells the low table function that the 0 line in the csv file has to the columns names 
+        // Instantiate star object from every row from the table 
+    }
+
+
     public void settings()
     {
         size(500, 500);
