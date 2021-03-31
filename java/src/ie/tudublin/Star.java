a4ccfba4d3eab3bac29aa31b9ad6c00fb4744535
Jina Park <jinapark119@gmail.com>
2021-03-31 15:41:20 +0100

    edit

--- java/src/ie/tudublin/Star.java
+++ java/src/ie/tudublin/Star.java
@@ -1,120 +1,74 @@
 package ie.tudublin;
 
-import processing.core.PApplet;
-import processing.data.TableRow;
-
-public class Star {
+// Encapsulate the interest of columns of interest into the fields of the class
+// Class has fields and methods
+// Fields being things that the class is 
+// Methods beigng things that the class does 
+// Right click -> Source action -> Generate Getters and Setters -> Tick everything and tada
+// VSC will write those accessor methods
+public class Star
+{
+    // All private fields -> They can only be accessed by methods inside the star class
     private boolean hab;
     private String displayName;
     private float distance;
-    private float xG,yG,zG;
+    private float xG, yG, zG;
     private float absMag;
-
-    public Star()
+    public boolean isHab()
     {
-        
-    }
-
-    public void render(PApplet pa)
-    {
-        float border = pa.width * 0.1f;
-        float x = PApplet.map(xG, -5, 5, border, pa.width - border);
-        float y = PApplet.map(yG, -5, 5, border, pa.width - border);
-        pa.stroke(255, 255, 0);
-        pa.line(x - 5, y, x + 5, y);
-        pa.line(x, y - 5, x, y + 5);
-        pa.stroke(255, 0, 0);
-        pa.noFill();
-        pa.circle(x, y, absMag);
-        pa.fill(255);
-        pa.textAlign(PApplet.LEFT, PApplet.CENTER);
-        pa.text(displayName, x + 10, y);
-    }
-
-    public Star(TableRow row)
-    {
-        // Constructor chaining
-        this(
-            row.getInt("Hab?") == 1 ? true : false,
-            row.getString("Display Name"),
-            row.getFloat("Distance"),
-            row.getFloat("Xg"),
-            row.getFloat("Yg"),
-            row.getFloat("Zg"),
-            row.getFloat("AbsMag")
-        );
-    }
-
-    public Star(boolean hab, String displayName, float distance, float xG, float yG, float zG, float absMag)
-    {
-        this.hab = hab;
-        this.displayName = displayName;
-        this.distance = distance;
-        this.xG = xG;
-        this.yG = yG;
-        this.zG = zG;
-        this.absMag = absMag;
-    }
-    
-    public boolean isHab() {
         return hab;
     }
 
-    public void setHab(boolean hab) {
+    public void setHab(boolean hab) // Assigning the pravate field from a parameter
+    {
         this.hab = hab;
     }
-
-    public String getDisplayName() {
+    public String getDisplayName()
+    {
         return displayName;
     }
-
-    public void setDisplayName(String displayName) {
+    public void setDisplayName(String displayName)
+    {
         this.displayName = displayName;
     }
-
-    public float getDistance() {
+    public float getDistance()
+    {
         return distance;
     }
-
-    public void setDistance(float distance) {
+    public void setDistance(float distance)
+    {
         this.distance = distance;
     }
-
-    public float getxG() {
+    public float getxG()
+    {
         return xG;
     }
-
-    public void setxG(float xG) {
+    public void setxG(float xG)
+    {
         this.xG = xG;
     }
-
-    public float getyG() {
+    public float getyG()
+    {
         return yG;
     }
-
-    public void setyG(float yG) {
+    public void setyG(float yG)
+    {
         this.yG = yG;
     }
-
-    public float getzG() {
+    public float getzG()
+    {
         return zG;
     }
-
-    public void setzG(float zG) {
+    public void setzG(float zG)
+    {
         this.zG = zG;
     }
-
-    public float getAbsMag() {
+    public float getAbsMag()
+    {
         return absMag;
     }
-
-    public void setAbsMag(float absMag) {
+    public void setAbsMag(float absMag)
+    {
         this.absMag = absMag;
-    }
-
-    @Override
-    public String toString() {
-        return "Star [absMag=" + absMag + ", displayName=" + displayName + ", distance=" + distance + ", hab=" + hab
-                + ", xG=" + xG + ", yG=" + yG + ", zG=" + zG + "]";
     }
 }
