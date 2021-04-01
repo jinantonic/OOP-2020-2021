794ea5ddb7c85e1161111bc1a8d4720fd3862372
Jina Park <jinapark119@gmail.com>
2021-04-01 16:02:22 +0100

    edit

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -1,13 +1,29 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class Gantt extends PApplet {
+
+    ArrayList<Task> task = new ArrayList<Task>();
 
     public void setup()
     {
         
     }
+
+    void loadTasks()
+    {
+        Table table = loadTable("Task.csv", "header");
+        for(TableRow row:table.rows())
+        {
+            
+        }
+    }
+
 
     public void settings()
     {
