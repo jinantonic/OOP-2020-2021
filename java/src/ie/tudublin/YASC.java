16d4e91244d50adcfe218c604c69aa4506b40ccf
Jina Park <jinapark119@gmail.com>
2021-04-07 16:55:56 +0100

    blah

--- java/src/ie/tudublin/YASC.java
+++ java/src/ie/tudublin/YASC.java
@@ -2,54 +2,62 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class YASC extends PApplet {
-    boolean[] keys = new boolean[1024];
+public class YASC extends PApplet// Name has to match the file name 
+{
+    boolean[] keys = new boolean[102];
 
+    Player p, p1;
     
-    // Update your forks!
-    // Create a branch for today monday9
-    // Write drawPlayer
-    // Write movePlayer
-
-    Player p;
-
-    public void settings() {
+    public void settings()
+    {
         size(500, 500);
     }
 
-    public void setup() {
-        p = new Player(this, width / 2, height / 2);
+    public void setup()
+    {
+        p = new Player(this, width / 2, height / 2); // 1st parameter is reference to the PApplet so this 
+        p1 = new Player(this, 100, 100);
     }
 
-    public void draw() {
+    public void draw()
+    {
         background(0);
         stroke(255);
+
+        // Separate instances of the player class
+        // Player is encapsulated into this player object that does player stuff
         p.update();
         p.render();
-        
-        // Check collisions        
-        checkCollisions();
+        p1.update();
+        p1.render();
+
     }
 
-    void checkCollisions() 
+    // This is how you can check multiple keys being held down at the same time and also there's no delay now with the keys 
+    // Declare a boolean aray keys[] first
+    // You pass in the keys (k) and it checks the boolean array at position k or the upper case version of it 
+    boolean checkKey(int k)
     {
-    }
-
-    boolean checkKey(int k) {
-        if (keys.length >= k) {
+        if (keys.length >= k)
+        {
             return keys[k] || keys[Character.toUpperCase(k)];
         }
         return false;
     }
 
-    public void mousePressed() {
+    public void mousePressed()
+    {
+
     }
 
-    public void keyPressed() {
-        keys[keyCode] = true;
+    public void keyPressed() // Assigns the keys array at position keyCode to be true
+    {
+        keys[keyCode] = true; // keyCode -> what key was pressed
     }
 
-    public void keyReleased() {
+    public void keyReleased() // Assigns the keys array at position keyCode to be false
+    {
         keys[keyCode] = false;
     }
+
 }
