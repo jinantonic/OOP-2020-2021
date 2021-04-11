4e89ea7d54eee915016858c76fc2ee038fffc6f9
Jina Park <jinapark119@gmail.com>
2021-04-11 23:37:44 +0100

    bla

---
+++ java/src/D19123917/Main.java
@@ -0,0 +1,18 @@
+package D19123917;
+
+
+public class Main
+{	
+
+	public void startUI()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new JinasVisual());		
+	}
+
+	public static void main(String[] args)
+	{
+		Main main = new Main();
+		main.startUI();			
+	}
+}
