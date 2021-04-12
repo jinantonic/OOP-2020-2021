76fa22c69ca62d2b91de20f16bf1145310ae552d
Jina Park <jinapark119@gmail.com>
2021-04-13 00:23:24 +0100

    ewe

--- java/src/D19123917/JinasVisual.java
+++ java/src/D19123917/JinasVisual.java
@@ -9,13 +9,13 @@ public class JinasVisual extends Visual
     public void settings()
     {
         //size(512, 512, P3D);
-        size(512, 512);
+        size(800, 800, P3D);
     }   
 
     public void setup()
     {
         startMinim();
-        //surface.setResizable(true);
+        surface.setResizable(true);
         loadAudio("heroplanet.mp3");
         colorMode(HSB);
     }
@@ -51,6 +51,8 @@ public class JinasVisual extends Visual
         float average = 0;
         float sum = 0;
 
+        float halfW = width / 2;
+        float halfH = height / 2;
 
         // Iterate over all the elementsthe audio buffer
         for(int i = 0; i < getAb().size(); i++) // ab is an array list of audio buffer so ab.size() gives us the size of array buffer
@@ -66,24 +68,34 @@ public class JinasVisual extends Visual
             case 0: 
             {
                 float c = map(average, 0, 1, 0, 255);
-                noStroke();
-                fill(c, 255, 255);        
-             
-                // See the difference lerping makes? It smooths out the jitteryness of average, so the visual looks smoother
-                //ellipse(width / 4, 100, 50 + average * 500, 50 + average * 500);
-                ellipse(width / 2, height / 2, 300 + (lerpedAverage * 500), 300 + (lerpedAverage * 500));                
                 
                 noStroke(); 
-                fill(255);  
-                ellipse(width / 2 - 50, height / 2 - 150, 80 + (lerpedAverage * 500), 80 + (lerpedAverage * 500)); // Elmo eye balls
-                ellipse(width / 2 + 50, height / 2 - 150, 80 + (lerpedAverage * 500), 80 + (lerpedAverage * 500));
-                fill(0);
-                ellipse(width / 2 - 50, height / 2 - 150, 20 + (lerpedAverage * 500), 20 + (lerpedAverage * 500));// Elmo pupils
-                ellipse(width / 2 + 50, height / 2 - 150, 20 + (lerpedAverage * 500), 20 + (lerpedAverage * 500));
-                fill(33,363,234);
-                ellipse(width / 2, height / 2 - 90, 70 + (lerpedAverage * 500), 90 + (lerpedAverage * 500)); // Elmo nose
-                               
                 
+                fill(c, 255, 255); // Elmo arms
+                rect(halfW - 200, halfH - 55, 400 + (lerpedAverage * 500), 70 + (lerpedAverage * 500), 18);
+
+                fill(c, 255, 255); // Elmo upper body
+                arc(halfW, halfH + 250, 300 + (lerpedAverage * 500), 700 + (lerpedAverage * 500), PI, TWO_PI);
+
+                fill(c, 255, 255); // Elmo face    
+                ellipse(halfW, halfH - 200, 300 + (lerpedAverage * 500), 300 + (lerpedAverage * 500));                
+
+                fill(255); // Elmo eye balls
+                ellipse(halfW - 50, halfH - 350, 80 + (lerpedAverage * 500), 80 + (lerpedAverage * 500)); 
+                ellipse(halfW + 50, halfH - 350, 80 + (lerpedAverage * 500), 80 + (lerpedAverage * 500));
+
+                fill(0); // Elmo pupils
+                ellipse(halfW - 50, halfH - 350, 20 + (lerpedAverage * 500), 20 + (lerpedAverage * 500));
+                ellipse(halfW + 50, halfH - 350, 20 + (lerpedAverage * 500), 20 + (lerpedAverage * 500));
+
+                fill(33,363,234); // Elmo nose
+                ellipse(halfW, halfH - 300, 70 + (lerpedAverage * 500), 90 + (lerpedAverage * 500)); 
+
+                fill(0); // Elmo mouth
+                arc(halfW, halfH - 180, 250 + (lerpedAverage * 500), 220 + (lerpedAverage * 500), 0, PI); 
+                /*quad(halfW - 40, halfH - 80, halfW - 80, halfH, halfW / 2, halfH, halfW / 2, halfH - 80); // Left arm
+                quad(halfW + 40, halfH - 80, halfW + 80, halfH, halfW / 2, halfH, halfW + (halfW / 2), halfH - 80); // Right arm*/
+
                 
                 
                 
--- java/src/D19123917/Main.java
+++ java/src/D19123917/Main.java
@@ -12,7 +12,7 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		Main main = new Main();
+		Main main = new Main();   
 		main.startUI();			
 	}
 }
