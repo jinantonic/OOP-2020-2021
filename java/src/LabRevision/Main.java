e8e80e8abd1d255d62ce7ab5988f3a1663f80e14
Jina Park <jinapark119@gmail.com>
2021-04-19 16:09:43 +0100

    bla

---
+++ java/src/LabRevision/Main.java
@@ -0,0 +1,17 @@
+package LabRevision;
+
+public class Main
+{
+
+    public void gantt()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Gantt());
+    }
+    
+    public static void main(String[] arg)
+    {
+        Main main = new Main();
+		main.gantt();        
+    }
+}
