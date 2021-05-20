95a26a49a84694b99d4e876b46f056fcc5f5d39f
Jina Park <jinapark119@gmail.com>
2021-05-20 01:10:17 +0100

    bla

--- java/src/ie/tudublin/Star.java
+++ java/src/ie/tudublin/Star.java
@@ -3,17 +3,49 @@ package ie.tudublin;
 import processing.core.PApplet;
 import processing.data.TableRow;
 
+// CSV -> Comment seperated values file 
+// A text file as oppose to a binary file(img, mp3 file)
+// Every coloum is speerated with comma and then evey row is seperated with carriage return
+// Every line on this file corresponds to row of data and every comma delimits the column
+// Very first line contains the heading 
+
+
+// Encapsulate the interest of columns of interest into the fields of the class
+// Class has fields and methods
+// Fields being things that the class is 
+// Methods beigng things that the class does 
+// Right click -> Source action -> Generate Getters and Setters -> Tick everything and tada
+// VSC will write those accessor methods
 public class Star
 {
+    // All private fields -> They can only be accessed by methods inside the star class
     private boolean hab;
     private String displayName;
     private float distance;
     private float xG, yG, zG;
     private float absMag;
 
-    public Star(TableRow row) // Constructor
+    // 3 state system 
+    // 1. Haven't clicked anything
+    // 2. Clicked one star and want to draw the line to one star to the next star
+    // 3. Have clicked two stars
+
+    // Constructor -> The method that gets called when the object gets created
+    // They don't have a return type 
+    public Star() // Defalut constructor -> constructor that takes no parameters
     {
-        this
+
+    }
+
+
+    // Constructor that takes the table row and creates the star from a table row 
+    public Star(TableRow row)
+    {
+        // Instantiate the star from a single row from the table
+        // 1st, we have to see how to get columns from this row
+        // 2nd, we have to look at how to do the concept in java called constructor chaining 
+        // -> When you have loads of different constructors and u don't want to duplicate the code between the constructors so chain those constructors together 
+        this // Call this constructor from this constructor 
         (
             row.getInt("Hab?") == 1 ? true : false,
             row.getString("Display Name"),
@@ -22,37 +54,43 @@ public class Star
             row.getFloat("Yg"),
             row.getFloat("Zg"),
             row.getFloat("AbsMag")
-        );
+        ); // The constructor below is getting called from this constructor and we can cosntruct the star by passing in row from the table
     }
 
+    // Render each one of the stars onto the grid 
     public void render(PApplet pa)
     {
+        // The code below wouldn't work bc the processign libraries are stored in PApplet so have to pass papplet as a parameter
+        //float border = width * 0.1f;
+        //float x = map(xG, -5, 5, border, width - border);
+        //stroke(255, 255, 0);
+
         float border = pa.width * 0.1f;
-        float x = PApplet.map(xG, -5, 5, border, pa.width - border);
-        float y = PApplet.map(yG, -5, 5, border, pa.height - border);
+        float x = PApplet.map(xG, -5, 5, border, pa.width - border); // It's calling a method on the class PApplet rather than the instance PApplet -> map is static method
+        float y = PApplet.map(yG, -5, 5, border, pa.width - border);
         pa.stroke(255, 255, 0);
         pa.line(x - 5, y, x + 5, y);
         pa.line(x, y - 5, x, y + 5);
-
         pa.stroke(255, 0, 0);
         pa.noFill();
-        pa.circle(x, y, absMag);
-
-        pa.fill(255, 255, 255);
+        pa.circle(x, y, absMag); // width, height, size
+        pa.fill(255);
         pa.textAlign(PApplet.LEFT, PApplet.CENTER);
         pa.text(displayName, x + 10, y);
 
     }
 
-    public Star(boolean hab, String displayName, float distance, float xG, float yG, float zG, float absMag)
+    public Star(boolean hab, String displayName, float distance, float xG, float yG, float zG, float absMag)// Parameterised constructor which will take the default values for all of those fileds
     {
+        // Now take all those values above and assign them to the fileds in the class
+        // We can disambiguate between the filed hab and the parameter hab by using the keyword, this
         this.hab = hab;
         this.displayName = displayName;
         this.distance = distance;
         this.xG = xG;
         this.yG = yG;
-        this.zG = zG;
-        this.absMag = absMag;
+        this.zG = zG; 
+        this.absMag = absMag;// Now all of the fields are assigned 
     }
 
     public boolean isHab()
@@ -60,74 +98,67 @@ public class Star
         return hab;
     }
 
-    public void setHab(boolean hab)
+    public void setHab(boolean hab) // Assigning the pravate field from a parameter
     {
         this.hab = hab;
     }
-
     public String getDisplayName()
     {
         return displayName;
     }
-
     public void setDisplayName(String displayName)
     {
         this.displayName = displayName;
     }
-
     public float getDistance()
     {
         return distance;
     }
-
     public void setDistance(float distance)
     {
         this.distance = distance;
     }
-
     public float getxG()
     {
         return xG;
     }
-
     public void setxG(float xG)
     {
         this.xG = xG;
     }
-
     public float getyG()
     {
         return yG;
     }
-
     public void setyG(float yG)
     {
         this.yG = yG;
     }
-
     public float getzG()
     {
         return zG;
     }
-
     public void setzG(float zG)
     {
         this.zG = zG;
     }
-
     public float getAbsMag()
     {
         return absMag;
     }
-
     public void setAbsMag(float absMag)
     {
         this.absMag = absMag;
     }
 
-    @Override
-    public String toString()
+    // toString method is the method that gets called automatically whenever you pass this object to something that expects a string
+    // Source action -> generate toString()
+    /*public String toString()
     {
+        return hab + "\t" + displayName + "\t" + distance + "\t" + xG + "\t" + yG + "\t" + zG + "\t" + absMag;
+    }*/
+    @Override
+    public String toString() {
         return "Star [absMag=" + absMag + ", displayName=" + displayName + ", distance=" + distance + ", hab=" + hab
                 + ", xG=" + xG + ", yG=" + yG + ", zG=" + zG + "]";
     }
--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -8,42 +8,40 @@ import processing.data.TableRow;
 
 public class StarMap extends PApplet
 {
-    ArrayList<Star> stars = new ArrayList<Star>();
+    // Load an array list of start object
+    // Declare at the array list 
+    // crtl + . is automatic code fixing in VSC
+    // This is an empty arraylist that currently has no elements in it
+    ArrayList<Star> stars = new ArrayList<Star>(); // Array list holding the star object (generic -> passing a type to a parameter)
 
     int startStar = -1;
     int endStar = -1;
 
-
+    // Draw grid
     void drawGrid()
     {
-        float border = width * 0.1f;
+        float border = 0.1f * width; 
         textAlign(CENTER, CENTER);
-        for(int i = -5; i <= 5; i++)
+        //float drawable = width - (border * 2.0f);
+        //float gap = drawable / 10.0f;
+        for(int i = -5; i <= 5; i ++) // Generate numbers from -5 to 5
         {
             float x = map(i, -5, 5, border, width - border);
-            float y = map(i, -5, 5, border, width - border);
-            stroke(204, 0, 204);
-            line(x, border, x, height - border);
-            line(border, y, width - border, y);
+            float y = map(i, -5, 5, border, height - border);
+            //float x = border + ((i + 5) * gap);
+            //float y = border + ((i + 5) * gap);
 
-            fill(204, 0, 204);
+            stroke(0, 0, 255);
+            line(x, border, x, height - border); // x, border -> x down border 
+            line(border, y, width - border, y);
+            fill(255); 
             text(i, x, border / 2);
             text(i, border / 2, y);
-            
         }
     }
 
-    void loadData()
-    {
-        Table table = loadTable("HabHYG15ly.csv", "header");
 
-        for(TableRow row:table.rows())
-        {
-            Star s = new Star(row);
-            stars.add(s);
-        }   
-    }
-
+    // Print out all of those elements in the arraylist 
     void printStars()
     {
         for(Star s: stars)
@@ -52,91 +50,120 @@ public class StarMap extends PApplet
         }
     }
 
-    public void drawStars()
+
+    // Load the csv file into this arraylist 
+    void loadStars()
     {
-        for(Star s: stars)
+        // Create a table object, table is one of the processing objects 
+        Table table = loadTable("HabHYG15ly.csv", "header"); // 1st -> name of the csv file, 2nd -> tells the low table function that the 0 line in the csv file has to the columns names 
+        // Instantiate star object from every row from the table 
+        for(TableRow row:table.rows()) // Enhanced for loop -> Iterating over the table row by row
         {
-            s.render(this);
+            Star s = new Star(row); // Create a new star
+            stars.add(s); // Add the star to the arraylist 
         }
     }
+
 
     public void settings()
     {
         size(800, 800);
     }
-    
 
+    float border;
 
-    public void mouseClicked()
+    // If the mouse is clikcked here, it's iterating over and i'm finding the distance and so on
+    public void mouseClicked() // Gets called when your mouse is on the screen
     {
-       for(int i = 0; i < stars.size(); i++)
-       {
-           Star s = stars.get(i);
-           float x = map(s.getxG(), -5, 5, border, width - border);
-           float y = map(s.getyG(), -5, 5, border, height - border);
-           if(dist(mouseX, mouseY, x, y) < s.getAbsMag() / 2)
-           {
-                if(startStar == -1)
+        
+        //println("Mouse clicked");
+        // I have to know which star is clicked
+        // Iterate through all of the stars and see which star is clicked 
+        for(int i = 0; i < stars.size(); i ++)
+        {
+            Star s = stars.get(i); // Index into an arraylist as oppose to an array  
+            // I'm taking xG and yG fields of each star and mapping it from the range from -5 to 5 and mapping it on to from border to width-border and height-border
+            // So that's calculating the centre point of each star
+            float x = map(s.getxG(), -5, 5, border, width - border);
+            float y = map(s.getyG(), -5, 5, border, height - border);
+            // And then i'm checking to see if the distance between mouse X and Y and the x and y codinates is less than the radius of the star = that means i clicked inside of the circle
+            // Distance from the mouse to the centre point of the star to see if it's lesser than the radius of the star 
+            if(dist(mouseX, mouseY, x, y) < s.getAbsMag() / 2)
+            {
+                // Now i have to see is it the first star or the second star clicked? 
+                if(startStar == -1) // I haven't clicked any star yet 
                 {
-                    startStar = i;
+                    startStar = i; // Clicked on the first star 
                     break;
                 }
-                else if(endStar == -1)
+                else if(endStar == -1) // The startStar is clicked already but the endStar hasn't been clicked
                 {
-                    endStar = i;
+                    endStar = i; // Clicked on the second star 
                     break;
                 }
-                else
+                else // startStar != -1 && endStar != -1 -> Already clicked on 2 stars
                 {
+                    // Now i clicked the new startStar
                     startStar = i;
                     endStar = -1;
                 }
-           }
-       }
+                //println(s.getDisplayName()); // Print out the displayName 
+                //break;
+            }
+            
+        }
     }
-    
-    float border;
+
     public void setup()
     {
         border = width * 0.1f;
         colorMode(RGB);
-        loadData();
+        loadStars();
         printStars();
         
     }
 
-   
+    public void drawStars()
+    {
+        for(Star s: stars)
+        {
+            s.render(this); // this points to the current object
+        }
+    }
+
     public void draw()
     {
        background(0);
        drawGrid();
        drawStars();
-
-       if(startStar != -1 && endStar == -1)
+       if(startStar != -1 && endStar == -1) // I have clicked the startStar but endStar
        {
+            // Then draw the line between the startStar and the endStar
             Star s = stars.get(startStar);
             stroke(255, 255, 0);
+            // Calculate the centre of the star
             float x = map(s.getxG(), -5, 5, border, width - border);
             float y = map(s.getyG(), -5, 5, border, height - border);
             line(x, y, mouseX, mouseY);
-       }
-       else if(startStar != -1 && endStar != -1)
-       {
-            Star s1 = stars.get(startStar);
+        }
+        else if(startStar != -1 && endStar != -1) // I have clicked on both stars
+        {
+            Star s = stars.get(startStar); // 1st star
             stroke(255, 255, 0);
-            float x1 = map(s1.getxG(), -5, 5, border, width - border);
-            float y1 = map(s1.getyG(), -5, 5, border, height - border);
+            // Need to draw the line between 2 stars
+            float x1 = map(s.getxG(), -5, 5, border, width - border);
+            float y1 = map(s.getyG(), -5, 5, border, height - border);
 
-            Star s2 = stars.get(endStar);
+            Star s1 = stars.get(endStar); // 2nd star
             float x2 = map(s1.getxG(), -5, 5, border, width - border);
             float y2 = map(s1.getyG(), -5, 5, border, height - border);
             line(x1, y1, x2, y2);
 
-            float dist = dist(s1.getxG(), s1.getyG(), s1.getzG(), s2.getxG(), s2.getyG(), s2.getzG());
+            // Print the distance between 2 stars
+            float dist = dist(s.getxG(), s.getyG(), s.getzG(), s1.getxG(), s1.getyG(), s1.getzG());
             stroke(255);
             textAlign(CENTER, CENTER);
-            text("DIstance from " + s1.getDisplayName() + " to " + s2.getDisplayName() + " is " + dist + " parsecs", width / 2, height - (border /  2));
-
-       }
+            text("Distance between " + s.getDisplayName() + " and " + s1.getDisplayName() + " is " + dist + " parsecs", width / 2, height - (border / 2));
+        }
     }
 }
